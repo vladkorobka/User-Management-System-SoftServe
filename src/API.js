a8ef48af8ed5c32fd93505b47bcf4280dd95afc4
Vlad Korobka <vladseipa@gmail.com>
2022-03-28 13:49:44 +0300

    Rename RedirectToDepartments component

--- src/API.js
+++ src/API.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const API = {
   baseHeaders: {
     headers: {
-      Authorization: localStorage.getItem(process.env.REACT_APP_TOKEN_NAME) // process.env.REACT_APP_TOKEN
+      Authorization: localStorage.getItem(process.env.REACT_APP_TOKEN_NAME)
     }
   },
 
--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import PageError from './PageError/PageError'
 import PrivateRoute from './PrivateRoute/PrivateRoute'
-// import PrivateRouteLogin from './PrivateRouteLogin/PrivateRouteLogin'
 
 import Login from './Login/Login'
 
 const Departments = React.lazy(() => import ('./Departments/Departments'))
 const Department = React.lazy(() => import ('./Department/Department'))
 
-function redirectToDepartments () {
+function RedirectToDepartments () {
   return <Redirect to='/departments' />
 }
 
@@ -21,10 +20,10 @@ function App () {
           <Route path='/login' component={Login} />
           <Suspense fallback={<div>Loading</div>}>
             <Switch>
-            <PrivateRoute exact path='/' component={redirectToDepartments} />
+              <PrivateRoute exact path='/' component={RedirectToDepartments} />
               <PrivateRoute path='/departments/:id' component={Department} />
               <PrivateRoute path='/departments' component={Departments} />
-              <Route path='/*' component={PageError} />
+              <Route path='*' component={PageError} />
             </Switch>
           </Suspense>
         </Switch>
